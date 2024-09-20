618ded2ddd6a8e6654a9865f9500610d3f199e98
RaevenCowell <156131617+RaevenCowell@users.noreply.github.com>
2024-09-20 19:46:12 +0100

    new layout v5

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
     const skills = [
-        { id: 'skill1', name: 'HTML', icon: 'images/html5.png' },
-        { id: 'skill2', name: 'Skill 2', icon: 'https://github.com/RaevenCowell/RaevenCowell.github.io/blob/06489d937e971f0974f8dae39f4d1b263f2d7b7e/images/bootstrap.pnghttps://github.com/RaevenCowell/RaevenCowell.github.io/blob/06489d937e971f0974f8dae39f4d1b263f2d7b7e/images/bootstrap.png?raw=true' },
-        { id: 'skill3', name: 'Skill 3', icon: 'path/to/icon3.png' },
+        { id: 'skill1', name: 'HTML', icon: 'images/html5-icon-12117.png' },
+        { id: 'skill2', name: 'CSS', icon: 'images/CSS3.png' },
+        { id: 'skill3', name: 'JavaScript', icon: 'images/JavaScript.png' },
+        { id: 'skill4', name: 'Visual Studio Code', icon: 'images/Visual Studio Code (VS Code).png' },
+        { id: 'skill5', name: 'BootStrap', icon: 'images/Bootstrap.png' },
+        { id: 'skill6', name: 'Microsoft Office 365', icon: 'images/Microsoft_365_29.png' },
+        { id: 'skill7', name: 'Microsoft Excel', icon: 'images/xls-icon-3392.png' },
+        { id: 'skill8', name: 'MySQL', icon: 'images/MySQL.png' },
+        { id: 'skill9', name: 'Tableau', icon: 'images/Tableau-Icon.png' },
+        { id: 'skill10', name: 'PowerBI', icon: 'images/power-bi-icon.png' },
+        { id: 'skill11', name: 'GitHub', icon: 'images/github-6980894_1280.png' },
+        { id: 'skill12', name: 'Azure', icon: 'images/Microsoft_Azure.svg.png' },
         // Add more skills as needed
     ];
 
     const experiences = [
-        { title: 'IT Support Engineer', company: 'Alpha Global', description: 'Description of job 1' },
-        { title: 'Data Analyst Trainee', company: 'Just IT Skills Bootcamp', description: 'Description of job 2' },
-        { title: 'IT Support Officer: User Access Team Leader', company: 'Shared Technology Services', description: 'Description of job 3' },
+        { id: 'job1', title: 'IT Support Engineer', company: 'Alpha Global', description: '03/2024 – 09/2024' },
+        { id: 'job2', title: 'Data Analyst Trainee', company: 'Just IT Skills Bootcamp', description: '12/2023 – 03/2024' },
+        { id: 'job3', title: 'IT Support Officer: User Access Team Leader', company: 'Shared Technology Services', description: '07/2022 - 12/2023' },
+        { id: 'job4', title: 'IT Support Officer', company: 'Shared Technology Services', description: '12/2020 – 07/2022' },
+        { id: 'job5', title: 'IT Deployment Officer', company: 'Shared Technology Services', description: '12/2019 – 12/2020' },
+        { id: 'job6', title: 'Project Support Officer', company: 'Lewisham Council', description: '08/2018 – 12/2019' },
         // Add more experiences as needed
     ];
 
@@ -18,14 +30,14 @@ $(document).ready(function() {
         $(`#${skill.id} span`).text(skill.name);
     });
 
-    experiences.forEach(experience => {
-        $('.experience-container').append(`
-            <div class="experience">
-                <h3>${experience.title}</h3>
-                <p>${experience.company}</p>
-                <p>${experience.description}</p>
-            </div>
+  experiences.forEach(experience => {
+        $(`#${experience.id}`).html(`
+            <h3>${experience.title}</h3>
+            <p>${experience.company}</p>
+            <p>${experience.description}</p>
         `);
     });
+
+
     
 });
